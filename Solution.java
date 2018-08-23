2a7fdb2d30231f8a293dbba47296e4b0adb20eb8
Herok4Build <thomash.johnson261@gmail.com>
2018-08-23 10:23:41 -0400

    This is a solution to the match strings question on HackerRank.

---
+++ Solution.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the matchingStrings function below.
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        int string_index;
+        int queries_index;
+        int[] results = new int[queries.length];
+        for(queries_index = 0; queries_index<=queries.length-1; queries_index++)
+            {
+                 int counter = 0;
+                 for(string_index = 0; string_index <= strings.length-1; string_index++)
+                 {
+                     if (queries[queries_index].equals(strings[string_index]))
+                     {
+                         counter++;
+                     
+                         
+                     }
+                     if (string_index == strings.length-1)
+                     {
+                        results[queries_index] = counter;
+                     }
+                 }
+            }
+        return results;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
